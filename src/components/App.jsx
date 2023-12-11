61ee2694a6a8dd8d9b7261c982351666d877fd6d
박운성 <cheanju0@gmail.com>
2023-12-11 16:28:34 +0900

    Feat: router 추가
    router 추가 및 기본 page 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,34 @@
-function App() {}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Layout from '../Layout';
+import MainPage from '../pages/MainPage';
+import ListPage from '../pages/ListPage';
+import PostPage from '../pages/PostPage';
+import PostIdPage from '../pages/PostIdPage';
+import MessagePage from '../pages/MessagePage';
+import EditPage from '../pages/EditPage';
+import NotFoundPage from '../pages/NotFoundPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="list" element={<ListPage />} />
+          <Route path="post">
+            <Route index element={<PostPage />} />
+            <Route path="id">
+              <Route index element={<PostIdPage />} />
+              <Route path="message" element={<MessagePage />} />
+              <Route path="edit" element={<EditPage />} />
+            </Route>
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
