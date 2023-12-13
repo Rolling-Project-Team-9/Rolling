4ab670d7aacd03f1522ec56d2f203d1ca35304fe
kimbobby <148737398+kimbobby@users.noreply.github.com>
2023-12-13 17:33:59 +0900

    Feat: GNB  컴포넌트 디자인 및 기능 구현 (#5)
    * Feat: GNB 컴포넌트 디자인 및 기능 구현
    
    * Style: GNB 컴포넌트 관련 파일 디렉토리 주소 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import GlobalStyles from '../styles/globalStyle';
+import GNB from './GNB';
 
 function Layout() {
   return (
     <>
       <GlobalStyles />
-      <div>GNB</div>
+      <GNB />
       <main>
         <Outlet />
       </main>
