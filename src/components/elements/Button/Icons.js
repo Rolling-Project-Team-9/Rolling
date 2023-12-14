6992c156f6a2298c95b5b7ca900be3e3d57ba408
김윤미 <younmeet@gmail.com>
2023-12-14 16:38:27 +0900

    Fix: Button 컴포넌트 리팩토링

---
+++ src/components/elements/Button/Icons.js
@@ -0,0 +1,19 @@
+import addIcon from '../../../assets/icons/add.svg';
+import deleteIcon from '../../../assets/icons/delete.svg';
+import plusIcon from '../../../assets/icons/plus.svg';
+import editIcon from '../../../assets/icons/edit.svg';
+import closeIcon from '../../../assets/icons/close.svg';
+import checkIcon from '../../../assets/icons/check.svg';
+import shareIcon from '../../../assets/icons/share.svg';
+
+const ICONS = {
+  add: { img: addIcon, alt: '이모티콘 추가하기' },
+  plus: { img: plusIcon, alt: '롤링페이퍼 만들기' },
+  delete: { img: deleteIcon, alt: '롤링페이퍼 삭제하기' },
+  edit: { img: editIcon, alt: '메세지 수정하기' },
+  close: { img: closeIcon, alt: '닫기' },
+  check: { img: checkIcon, alt: '확인' },
+  share: { img: shareIcon, alt: '공유하기' },
+};
+
+export default ICONS;
