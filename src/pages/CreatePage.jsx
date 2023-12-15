85c0aff94b632d67a9184eb9700937537e9a69b7
JunCastle <juncastle97@gmail.com>
2023-12-15 17:07:01 +0900

    Feat: CreatePage 진행 중

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+import Option from '../components/create/Option';
+import Input from '../components/elements/Input';
+import Button from '../components/elements/Button/Button';
+import DESIGN_TOKEN from '../styles/tokens';
+
+const { color, typography } = DESIGN_TOKEN;
 
 function CreatePage() {
-  return <div>PostPage</div>;
+  return (
+    <div>
+      <InputContainer>
+        <Bold>To.</Bold>
+        <Input width="720" height="50" />
+      </InputContainer>
+      <TitleContainer>
+        <Bold>배경화면을 선택해 주세요.</Bold>
+        <Choice>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</Choice>
+      </TitleContainer>
+      <Option />
+      <Button variant="primary" width="720" height="x-large" type="button">
+        생성하기
+      </Button>
+    </div>
+  );
 }
+
+const Bold = styled.div`
+  ${typography.font24Bold}
+`;
+
+const Choice = styled.div`
+  color: ${color.gray[500]};
+  ${typography.font16Regular};
+`;
+
+const InputContainer = styled.div``;
+const TitleContainer = styled.div``;
 
 export default CreatePage;
