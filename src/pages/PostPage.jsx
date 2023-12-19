7c0774706622b0487c92e1bae2fba4c5d12d7304
JunCastle <juncastle97@gmail.com>
2023-12-19 15:26:38 +0900

    Fix: PostPage의 background min-height로 변경

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -63,7 +63,7 @@ export default PostPage;
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   background: ${(props) => {
     if (props.$bgImg) {
       return `url(${props.$bgImg})`;
